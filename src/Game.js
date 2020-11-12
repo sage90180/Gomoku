eed4a7135976fcb80af954f0c721298305a79641
sage90180 <sage90180@gmail.com>
2020-11-12 09:13:50 +0800

    pretty

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
-import Square from "./Square"
-import {useState} from "react"
-import calculateWinner from "./calculateWinner"
+import Square from "./Square";
+import { useState } from "react";
+import calculateWinner from "./calculateWinner";
 
 const GameWrap = styled.div``;
 const PlayerStatus = styled.div`
@@ -19,76 +19,78 @@ const PlayerStatus = styled.div`
     border: solid 2px white;
     padding: 5px 10px;
     letter-spacing: 2px;
-    box-shadow: 0px 0px 10px rgba(0,0,0,.4);
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.4);
   }
-`
+`;
 const BoardWrap = styled.div`
   width: 570px;
   height: 570px;
   background: white;
-  box-shadow: 0px 0px 10px rgba(0,0,0,.4);
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.4);
   display: flex;
   align-content: flex-start;
   flex-wrap: wrap;
   position: relative;
   box-sizing: border-box;
   &:after {
-    content: '';
+    content: "";
     position: absolute;
     left: 0px;
     top: 0px;
     width: 570px;
     height: 570px;
     border: solid 15px #777;
-    z-index: 1
+    z-index: 1;
   }
 `;
-
 function Game() {
-  const [squares, setSquares] = useState(Array(19).fill(Array(19).fill(null)))
-  const [xIsNext, setXisNext] = useState(true)
+  const [squares, setSquares] = useState(Array(19).fill(Array(19).fill(null)));
+  const [xIsNext, setXisNext] = useState(true);
+  // 計算是否有贏家
   let status;
   const winner = calculateWinner(squares);
   if (winner) {
-    status = '獲勝者：' + (winner==='black'?  '黑子':'白子')
+    status = "獲勝者：" + (winner === "black" ? "黑子" : "白子");
   } else {
-    status = '下一位： ' + (xIsNext ? '黑子' : '白子');
+    status = "下一位： " + (xIsNext ? "黑子" : "白子");
   }
-  const handleClick = (x,y,xIsNext) =>{
-  const newBoard = JSON.parse(JSON.stringify(squares))
-    setSquares(newBoard)
-    if(calculateWinner(newBoard)){
-      return
+  // 下棋
+  const handleChessClick = (x, y, xIsNext) => {
+    const newBoard = JSON.parse(JSON.stringify(squares));
+    setSquares(newBoard);
+    if (calculateWinner(newBoard)) {
+      return;
     }
-    if(newBoard[x][y] === null){
-      newBoard[x][y] = xIsNext? 'black':'white'
-      setXisNext(xIsNext=!xIsNext)
+    if (newBoard[x][y] === null) {
+      newBoard[x][y] = xIsNext ? "black" : "white";
+      setXisNext((xIsNext = !xIsNext));
     }
-  }
+  };
+  // 開新局
   const restart = () => {
-    setSquares(Array(19).fill(Array(19).fill(null)))
-    setXisNext(true)
-  }
+    setSquares(Array(19).fill(Array(19).fill(null)));
+    setXisNext(true);
+  };
   return (
     <GameWrap>
       <PlayerStatus>
         <div>{status}</div>
-        <div className='restart-btn' onClick={restart}>開新局</div>
+        <div className="restart-btn" onClick={restart}>
+          開新局
+        </div>
       </PlayerStatus>
       <BoardWrap>
-      {
-        squares.map((square, indexY)=>(
-          square.map((eachSquare,indexX)=>(
-            <Square 
-              key={indexX} 
-              value = {squares[indexX][indexY]}
-              handleClick={() => handleClick(indexX,indexY,xIsNext)}
-          />))
-        ))
-      }
+        {squares.map((square, indexY) =>
+          square.map((eachSquare, indexX) => (
+            <Square
+              key={indexX}
+              value={squares[indexX][indexY]}
+              handleClick={() => handleChessClick(indexX, indexY, xIsNext)}
+            />
+          ))
+        )}
       </BoardWrap>
     </GameWrap>
   );
 }
 export default Game;
-
--- src/Square.js
+++ src/Square.js
@@ -7,59 +7,71 @@ const SquareWrap = styled.div`
   z-index: 999;
   cursor: crosshair;
   &:hover {
-    box-shadow: 0px 0px 10px rgba(0,0,0,.1);
-    background: rgba(0,0,0,.1);
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    background: rgba(0, 0, 0, 0.1);
     border-radius: 50px;
   }
-  &:before{
-    content: '';
+  &:before {
+    content: "";
     width: 100%;
     height: 1px;
     background: #777;
     position: absolute;
     top: 50%;
-    transform: translateY(-50%)
+    transform: translateY(-50%);
   }
-  &:after{
-    content: '';
+  &:after {
+    content: "";
     height: 100%;
     width: 1px;
     background: #777;
     position: absolute;
     left: 50%;
-    transform: translateX(-50%)
+    transform: translateX(-50%);
   }
   .white {
-    width: 23px;
-    height: 23px;
-    background: radial-gradient(circle at 35% 25%,
-        white 0%,#eee 30%,#ccc 60%,#bbb 80%,#aaa 95%,#999 100%);
+    width: 25px;
+    height: 25px;
+    background: radial-gradient(
+      circle at 35% 25%,
+      white 0%,
+      #eee 30%,
+      #ccc 60%,
+      #bbb 80%,
+      #aaa 95%,
+      #999 100%
+    );
     position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-50%,-50%);
+    transform: translate(-50%, -50%);
     border-radius: 25px;
     z-index: 997;
-    box-shadow: 0px 0px 5px rgba(0,0,0,.1)
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
   }
   .black {
     width: 23px;
     height: 23px;
-    background: radial-gradient(circle at 35% 25%,
-        #999 0%,#000 90%,#666 95%,#999 100%);
+    background: radial-gradient(
+      circle at 35% 25%,
+      #999 0%,
+      #000 90%,
+      #666 95%,
+      #999 100%
+    );
     position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-50%,-50%);
+    transform: translate(-50%, -50%);
     border-radius: 25px;
     z-index: 998;
-    box-shadow: 0px 0px 5px rgba(0,0,0,.3);
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
   }
 `;
 
-function Square({value,handleClick}) {
+function Square({ value, handleClick }) {
   return (
-    <SquareWrap onClick={handleClick}>  
+    <SquareWrap onClick={handleClick}>
       <div className={value}></div>
     </SquareWrap>
   );
